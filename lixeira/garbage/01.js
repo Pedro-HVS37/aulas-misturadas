9fda7f530e819a37c11741e15db5f99ee89316d8
Pedro <you@example.com>
2025-07-10 13:47:34 -0300

    feat: adding lixeira

---
+++ lixeira/garbage/01.js
@@ -0,0 +1,4 @@
+let a = 5, b = 10, c = 15, d = 20, bob = 4, media = (a + b + c + d) /bob;
+console.log("A media é:" + media);
+let x = 3; x += 4; y = 6; y -=3; z = 9; z /=5;
+console.log(x, y, z);
---
+++ lixeira/garbage/minilicao.js
@@ -0,0 +1,11 @@
+1
+let produto = 100, desconto = 20; total = produto-produto*(desconto/100);
+console.log("1: " + total);
+
+2
+let celsius = 23; fahrenheit = (celsius*9/5)+32;
+console.log("2: " + fahrenheit);
+
+3
+let a = 12; b = 12; c = 12; perimetro = a+b+c;
+console.log("Perímetro: " + perimetro);
